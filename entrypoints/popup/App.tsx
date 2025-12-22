eae2ca3a75c7eedf7a8f4694d0086c1c9a96bf30
Rumon360 <hmk.rumon@gmail.com>
2025-12-23 01:55:23 +0600

    chore: clean up pop up ui

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-zinc-800 border border-zinc-700 text-zinc-50 w-80">
+      <div className="p-4">
+        <h1>Hello World</h1>
+      </div>
     </div>
   );
 }

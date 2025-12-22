c3150519142fd11c3a58c4a226d58d9b6ca1d7df
Rumon360 <hmk.rumon@gmail.com>
2025-12-23 01:13:19 +0600

    chore: update info

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,12 @@ import path from "path";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "Zen Mode - Youtube",
+    description:
+      "Zen Mode - Youtube is a browser extension helps you to watch videos in Zen mode. 😌",
+    version: "0.1.0",
+  },
   vite: () => ({
     plugins: [tailwindcss()],
     resolve: {

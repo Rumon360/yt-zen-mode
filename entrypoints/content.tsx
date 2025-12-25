702fd1f05b5402fef6959a8dbbcb1fa27743c7f5
Rumon360 <hmk.rumon@gmail.com>
2025-12-26 01:37:21 +0600

    feat: implement conditional ZenUI injection on YouTube

--- entrypoints/content.tsx
+++ entrypoints/content.tsx
@@ -1,31 +1,70 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM, { Root } from "react-dom/client";
 import ZenUI from "@/components/zen-ui";
+import { ContentScriptContext } from "wxt/utils/content-script-context";
+import { ShadowRootContentScriptUi } from "wxt/utils/content-script-ui/shadow-root";
+import { storage } from "@wxt-dev/storage";
+
+const whereToShowUI = [new MatchPattern("*://*.youtube.com/")];
+
+let uiRef: ShadowRootContentScriptUi<Root> | null = null;
 
 export default defineContentScript({
-  matches: ["*://*.google.com/*"],
+  matches: ["*://*.youtube.com/*"],
   cssInjectionMode: "ui",
   runAt: "document_end",
   async main(ctx) {
     console.log("Hello content.");
 
-    const ui = await createShadowRootUi(ctx, {
-      name: "zen-mode-youtube",
-      position: "inline",
-      anchor: "body",
-      onMount: (container) => {
-        const app = document.createElement("div");
-        container.append(app);
-
-        const root = ReactDOM.createRoot(app);
-        root.render(<ZenUI />);
-
-        return root;
-      },
-      onRemove: (root) => {
-        root?.unmount();
-      },
+    const zenModeStatus = await storage.getItem("local:zen-mode-status", {
+      fallback: "not-activated",
     });
 
-    ui?.mount();
+    if (zenModeStatus === "activated") {
+      document.documentElement.style.setProperty(
+        "--zen-mode-status",
+        "activated"
+      );
+
+      ctx.addEventListener(window, "wxt:locationchange", async ({ newUrl }) => {
+        if (!uiRef && checkIfWeCanInjectUI(newUrl.toString())) {
+          await mounShadowRootUI(ctx);
+        }
+      });
+
+      if (checkIfWeCanInjectUI(window.location.href)) {
+        await mounShadowRootUI(ctx);
+      }
+    } else {
+      document.documentElement.style.setProperty(
+        "--zen-mode-status",
+        "not-activated"
+      );
+    }
   },
 });
+
+async function mounShadowRootUI(ctx: ContentScriptContext) {
+  uiRef = await createShadowRootUi(ctx, {
+    name: "zen-mode-youtube",
+    position: "inline",
+    anchor: "body",
+    onMount: (container) => {
+      const app = document.createElement("div");
+      container.append(app);
+
+      const root = ReactDOM.createRoot(app);
+      root.render(<ZenUI />);
+
+      return root;
+    },
+    onRemove: (root) => {
+      root?.unmount();
+    },
+  });
+
+  uiRef?.mount();
+}
+
+function checkIfWeCanInjectUI(newUrl: string) {
+  return whereToShowUI.some((pattern) => pattern.includes(newUrl));
+}

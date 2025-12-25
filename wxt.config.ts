9ba4f5e4f352c8a4ec826c1e897d8956f77c9bfe
Rumon360 <hmk.rumon@gmail.com>
2025-12-26 01:36:27 +0600

    feat: inject reset.css into YouTube via manifest hook

--- wxt.config.ts
+++ wxt.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     description:
       "Zen Mode - Youtube is a browser extension helps you to watch videos in Zen mode. 😌",
     version: "0.1.0",
+    permissions: ["tabs", "storage"],
   },
   vite: () => ({
     plugins: [tailwindcss()],
@@ -19,4 +20,13 @@ export default defineConfig({
       },
     },
   }),
+  hooks: {
+    "build:manifestGenerated": (wxt, manifest) => {
+      manifest.content_scripts = manifest.content_scripts || [];
+      manifest.content_scripts?.push({
+        css: ["assets/reset.css"],
+        matches: ["*://*.youtube.com/*"],
+      });
+    },
+  },
 });
